f7d445b59a4e59d81215ea5dd63b9c221f405f23
fengxing <f296016140@gmail.com>
2019-03-19 16:31:43 +0800

    RQ: 代码检查工具

--- plugin_base/src/main/java/com/component/fx/plugin_base/BaseApplication.java
+++ plugin_base/src/main/java/com/component/fx/plugin_base/BaseApplication.java
@@ -3,6 +3,9 @@ package com.component.fx.plugin_base;
 import android.app.Application;
 import android.content.Context;
 
+import com.facebook.stetho.Stetho;
+import com.squareup.leakcanary.LeakCanary;
+
 public abstract class BaseApplication extends Application {
 
     protected static Context mContext;
@@ -27,6 +30,17 @@ public abstract class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = this;
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+        }
+
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
 
